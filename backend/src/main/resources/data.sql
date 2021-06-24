74a208073c019b214e18cedbe38dbb27b997d0f9
andrezasecon <andrezasecon@gmail.com>
2021-06-24 16:26:52 -0300

    User, Role

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('BootCamp HTML', 'https://pxhere.com/pt/photo/1444653', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQTcZmdejs2FLUHAt_L7S7cBcsJVswNZXOEYrOTPtyz815cqWe2jQ5n2aZEvI9tGahyTY&usqp=CAU');
