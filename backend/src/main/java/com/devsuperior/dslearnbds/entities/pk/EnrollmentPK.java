8a6848136e9993d96d027f9f3ab480d61f9da8a1
andrezasecon <andrezasecon@gmail.com>
2021-06-25 13:48:56 -0300

    Enrollment, EnrollmentPK

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/pk/EnrollmentPK.java
@@ -0,0 +1,60 @@
+package com.devsuperior.dslearnbds.entities.pk;
+
+import com.devsuperior.dslearnbds.entities.Offer;
+import com.devsuperior.dslearnbds.entities.User;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class EnrollmentPK implements Serializable {
+    private static final long SerialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "offer_id")
+    private Offer offer;
+
+    public EnrollmentPK() {
+    }
+
+    public EnrollmentPK(User user, Offer offer) {
+        this.user = user;
+        this.offer = offer;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Offer getOffer() {
+        return offer;
+    }
+
+    public void setOffer(Offer offer) {
+        this.offer = offer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EnrollmentPK that = (EnrollmentPK) o;
+        return Objects.equals(user, that.user) && Objects.equals(offer, that.offer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user, offer);
+    }
+}
